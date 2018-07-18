638c65cd99a6e647aa8e5bb7b772753ca0520942
Revolt27 <Kuckushckin.alex@yandex.ru>
2018-07-18 23:42:11 +0300

    + Created View Palette, MVVM pattern classes

---
+++ MaterialWpfApplication/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using MaterialWpfApplication.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MaterialWpfApplication.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        MainWindow window = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+
+        public MainWindowViewModel()
+        {
+            OpenPaletteCommand = new DelegateCommand(o => OpenPalette());
+        }
+
+        #region Command
+
+        public DelegateCommand OpenPaletteCommand { get; set; }
+
+        #endregion
+
+        #region Command implementation
+
+        private void OpenPalette()
+        {
+            window.mainGrid.Children.Clear();
+            window.mainGrid.Children.Add(new View.Palette());
+        }
+
+        #endregion
+    }
+}
